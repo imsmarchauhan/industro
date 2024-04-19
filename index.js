8675424d6d7df70c1019e615636318e12675840c
imsmarchauhan <career.surajkumar@gmail.com>
2024-04-19 14:07:23 +0530

    Added nodemailer

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ const express = require("express");
 const hbs = require("hbs");
 const path = require("path");
 const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+const { log } = require("console");
 
 const app = express();
-const encoder = bodyParser.urlencoded()
+const encoder = bodyParser.urlencoded();
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    tls: true,
+    auth:{
+        user:"career.surajkumar@gmail.com",
+        pass: "vwlrwtitycsfvdot"
+    }
+})
 
 app.set('view engine', "hbs");
 app.use(express.static("public"));
@@ -20,11 +32,61 @@ app.get("/about",(req,res)=>{
 })
 
 app.get("/contact",(req,res)=>{
-    res.render("contact")
+    res.render("contact",{show: false})
 })
 app.post("/contact",encoder, (req,res)=>{
-    console.log(req.body);
-    res.render("contact")
+    let mailOptions = {
+        from : "career.surajkumar@gmail.com",
+        to: req.body.email,
+        subject: "Confirmation: Industro",
+        text: `
+                Hello ${req.body.name},
+                Your query has been received. Our team will connect your shortly!
+
+                Regards,
+                Team Industro 
+                `
+    }
+    transporter.sendMail(mailOptions, (error)=>{
+        console.log(error);
+    })
+    mailOptions = {
+        from : "career.surajkumar@gmail.com",
+        to: "career.surajkumar@gmail.com",
+        subject: "Query received : Industro",
+        html: `
+                <h3>Hi</h3>
+                <h3>Please find below the details of the query: </h3>
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>Name</th>
+                            <td>${req.body.name}</td>
+                        </tr>
+                        <tr>
+                            <th>Email</th>
+                            <td>${req.body.email}</td>
+                        </tr>
+                        <tr>
+                            <th>Phone</th>
+                            <td>${req.body.phone}</td>
+                        </tr>
+                        <tr>
+                            <th>Subject</th>
+                            <td>${req.body.subject}</td>
+                        </tr>
+                        <tr>
+                            <th>Message</th>
+                            <td>${req.body.message}</td>
+                        </tr>
+                    </tbody>
+                </table>
+        `
+    }
+    transporter.sendMail(mailOptions, (error)=>{
+        console.log(error);
+    })
+    res.render("contact",{show: true})
 })
 app.get("/feature",(req,res)=>{
     res.render("feature")
